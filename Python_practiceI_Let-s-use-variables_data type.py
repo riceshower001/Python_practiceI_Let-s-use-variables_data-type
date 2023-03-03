668e50d3659c87d0b62d2480558232af9ffc6bb4
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-03 17:52:14 +0900

    Add files via upload

---
+++ Python_practiceI_Let-s-use-variables_data type.py
@@ -0,0 +1,9 @@
+# Let's do type conversion.
+
+age = 24
+# Please output "I am 24 years old." using "age".
+print("I am "+str(age)+" years old.")
+
+count = '5'
+# Please print the value of "count" plus 1.
+print(int(count) + 1)
